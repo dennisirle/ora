4018d1d14d94d7c514a18a6e63579455e959dca5
unknown <dirle@FRANKWST>
2018-10-12 15:18:28 +0200

    Neuer Schnellbericht

--- IFSIRLE/Views/FWST_ABFRAGE.sql
+++ IFSIRLE/Views/FWST_ABFRAGE.sql
@@ -1,8 +1,8 @@
-CREATE OR REPLACE FORCE VIEW ifsirle.fwst_abfrage (contract,part_no,accounting_group,description) AS
+CREATE OR REPLACE FORCE VIEW ifsirle.fwst_abfrage (contract,part_no,accounting_group,description,part_cost_group_id) AS
 SELECT "CONTRACT",
        "PART_NO",
        "ACCOUNTING_GROUP",
-       t.description
-
+       t.description,
+       t.part_cost_group_id
   FROM IFSAPP.INVENTORY_PART T
 ;
