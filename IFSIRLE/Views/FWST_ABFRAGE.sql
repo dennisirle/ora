c71998bcab51fdb653d7f809ddda7ba568ac0002
unknown <dirle@FRANKWST>
2018-10-12 15:20:14 +0200

    Änderung für SWetter, Gewicht hinzugefügt

--- IFSIRLE/Views/FWST_ABFRAGE.sql
+++ IFSIRLE/Views/FWST_ABFRAGE.sql
@@ -1,8 +1,9 @@
-CREATE OR REPLACE FORCE VIEW ifsirle.fwst_abfrage (contract,part_no,accounting_group,description,part_cost_group_id) AS
+CREATE OR REPLACE FORCE VIEW ifsirle.fwst_abfrage (contract,part_no,accounting_group,description,part_cost_group_id,weight_net) AS
 SELECT "CONTRACT",
        "PART_NO",
        "ACCOUNTING_GROUP",
        t.description,
-       t.part_cost_group_id
+       t.part_cost_group_id,
+       t.weight_net
   FROM IFSAPP.INVENTORY_PART T
 ;
