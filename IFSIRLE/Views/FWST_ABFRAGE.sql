f2816767b97d07baec610c6812841f83a2ad23e5
unknown <dirle@FRANKWST>
2018-10-12 15:15:47 +0200

    Zusatzwunsch von DIrle

--- IFSIRLE/Views/FWST_ABFRAGE.sql
+++ IFSIRLE/Views/FWST_ABFRAGE.sql
@@ -1,7 +1,8 @@
-CREATE OR REPLACE FORCE VIEW ifsirle.fwst_abfrage (contract,part_no,accounting_group) AS
+CREATE OR REPLACE FORCE VIEW ifsirle.fwst_abfrage (contract,part_no,accounting_group,description) AS
 SELECT "CONTRACT",
        "PART_NO",
-       "ACCOUNTING_GROUP"
+       "ACCOUNTING_GROUP",
+       t.description
 
   FROM IFSAPP.INVENTORY_PART T
 ;
